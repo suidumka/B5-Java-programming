0e06c510ce306bbb58f4e4425fb9308f885fa284
suidumka <jalilova.s.m@gmail.com>
2025-05-23 20:28:59 -0400

    First Change

---
+++ src/Homework_day08/RateCalculator.java
@@ -0,0 +1,18 @@
+package Homework_day08;
+
+import java.util.Scanner;
+
+public class RateCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please, enter your annual salary: $");
+        double salary = input.nextDouble();
+        System.out.print("Enter how many hours per week you work: ");
+        double hoursPerWeek = input.nextDouble();
+        double result = salary / (hoursPerWeek * 52);
+        System.out.println("Hourly rate of the employee is: $" + result);
+
+
+    }
+}
+
